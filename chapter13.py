bb01569695d7e2995f7e749f2edd180f38f858ec
Sima <sima.soltani@queensu.ca>
2020-04-15 11:01:07 -0400

    page 450 Chapter13

--- chapter13.py
+++ chapter13.py
@@ -242,3 +242,20 @@ for  i ,(image,label) in enumerate (zip(images,labels)):
     ax.imshow(image)
     ax.set_title('{}'.format(label),size=15)
 plt.show()
+
+mnist,mnist_info = tfds.load('mnist',with_info = True,shuffle_files = False)
+print(mnist_info)
+print(mnist.keys())
+ds_train = mnist['train']
+ds_train = ds_train.map(lambda item:(item['image'],item['label']))
+ds_train = ds_train.batch(10)
+batch = next(iter(ds_train))
+print(batch[0].shape,batch[1])
+fig = plt.figure(figsize = (15,6))
+for i,(image,label) in enumerate(zip(batch[0],batch[1])):
+    ax=fig.add_subplot(2,5,i+1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.imshow(image[:,:,0],cmap='gray_r')
+    ax.set_title('{}'.format(label),size = 15)
+plt.show()
