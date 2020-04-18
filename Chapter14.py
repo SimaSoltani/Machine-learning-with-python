7cf00a043d7e0bb00814dec79fb5e9ee0779943a
Sima <sima.soltani@queensu.ca>
2020-04-18 14:11:30 -0400

    Page 483 Chapter14

--- Chapter14.py
+++ Chapter14.py
@@ -70,8 +70,70 @@ tf.print('Scalar Inputs:', compute_z(1,2,3))
                                             dtype=tf.int32),
                               tf.TensorSpec(shape=[None],
                                             dtype=tf.int32),))
-df compute_z(a,b,c):
+def compute_z(a,b,c):
     r1=tf.subtract(a,b)
     r2=tf.multiply(2,r1)
     z=tf.add(r2,c)
     return z
+
+tf.print('Rank 1 Input:', compute_z([1],[2],[3]))
+tf.print('Rank 1 Inputs:',compute_z([1,2],[2,4],[3,6]))
+
+# Tensorflow Variable objects for storing and updating model parameters
+a = tf.Variable(initial_value = 3.14, name ='var_a')
+print(a)
+b = tf.Variable(initial_value=[1,2,3], name ='var_b')
+print(b)
+c = tf.Variable(initial_value=[True,False],dtype=tf.bool)
+print(c)
+d=tf.Variable(initial_value=['abc'],dtype=tf.string)
+print(d)
+
+
+#Define a non-trainable variable
+w=tf.Variable([1,2,3],trainable=False)
+print(w.trainable)
+
+print(w.assign([3,1,4],read_value=True))
+w.assign_add([2,-1,2],read_value=False)
+print(w.value())
+
+
+#Create a Variable with Glorot initialization
+tf.random.set_seed(1)
+init=tf.keras.initializers.GlorotNormal()
+tf.print(init(shape=(3,)))
+
+#initialize a cvariable of shape 2x3
+v=tf.Variable(init(shape=(2,3)))
+tf.print(v)
+
+
+class MyModule(tf.Module):
+    def __init__(self):
+        init=tf.keras.initializers.GlorotNormal()
+        self.w1 = tf.Variable(init(shape=(2,3)),
+                              trainable=True)
+        self.w2 = tf.Variable(init(shape=(1,2)),
+                              trainable = False)
+
+
+m=MyModule()
+print('All module variables:',[v.shape for v in m.variables])
+print('Trainable variable:',[v.shape for v in m.trainable_variables])
+
+
+@tf.function
+def f(x):
+    w=tf.Variable([1,2,3])
+
+f([1])
+
+#define variable outside the decorated function and use it inside
+w = tf.Variable(tf.random.uniform((3,3)))
+@tf.function
+def compute_z(x):
+    return tf.matmul(w,x)
+
+x=tf.constant([[1],[2],[3]],dtype=tf.float32)
+tf.print(compute_z(x))
