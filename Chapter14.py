ac25549101b41ef72d2dd87e4e50117129405a04
Sima <sima.soltani@queensu.ca>
2020-04-20 11:43:44 -0400

    Page 494 Chapter14

--- Chapter14.py
+++ Chapter14.py
@@ -257,3 +257,155 @@ hist = model.fit(x_train,y_train,
 from mlxtend.plotting import plot_decision_regions
 
 history = hist.history
+fig = plt.figure(figsize=(16,4))
+ax = fig.add_subplot(1,3,1)
+plt.plot(history['loss'],lw=4)
+plt.plot(history['val_loss'],lw=4)
+plt.legend(['Train loss','Validation loss'],fontsize=15)
+ax.set_xlabel('Epochs',size=15)
+
+ax=fig.add_subplot(1,3,2)
+plt.plot(history['binary_accuracy'],lw=4)
+plt.plot(history['val_binary_accuracy'],lw=4)
+plt.legend(['Train acc.','Validation Acc.'],fontsize=15)
+ax.set_xlabel('Epochs',size=15)
+
+ax=fig.add_subplot(1,3,3)
+plot_decision_regions(X=x_valid,y=y_valid.astype(np.integer),
+                      clf=model)
+ax.set_xlabel(r'$x_1$',size=15)
+ax.xaxis.set_label_coords(1,-0.025)
+ax.set_ylabel(r'$x_2$',size=15)
+ax.yaxis.set_label_coords(-0.025,1)
+plt.show()
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(
+    units=4,
+    input_shape = (2,),
+    activation = 'relu'))
+
+model.add(tf.keras.layers.Dense(
+    units=1,
+    activation = 'sigmoid'))
+model.summary()
+
+model.compile (optimizer = tf.keras.optimizers.SGD(),
+               loss = tf.keras.losses.BinaryCrossentropy(),
+               metrics=[tf.keras.metrics.BinaryAccuracy()])
+hist = model.fit(x_train,y_train,
+                 validation_data = (x_valid,y_valid),
+                 epochs=200, batch_size = 2, verbose=0)
+
+history = hist.history
+fig = plt.figure(figsize=(16,4))
+ax = fig.add_subplot(1,3,1)
+plt.plot(history['loss'],lw=4)
+plt.plot(history['val_loss'],lw=4)
+plt.legend(['Train loss','Validation loss'],fontsize=15)
+ax.set_xlabel('Epochs',size=15)
+
+ax=fig.add_subplot(1,3,2)
+plt.plot(history['binary_accuracy'],lw=4)
+plt.plot(history['val_binary_accuracy'],lw=4)
+plt.legend(['Train acc.','Validation Acc.'],fontsize=15)
+ax.set_xlabel('Epochs',size=15)
+
+ax=fig.add_subplot(1,3,3)
+plot_decision_regions(X=x_valid,y=y_valid.astype(np.integer),
+                      clf=model)
+ax.set_xlabel(r'$x_1$',size=15)
+ax.xaxis.set_label_coords(1,-0.025)
+ax.set_ylabel(r'$x_2$',size=15)
+ax.yaxis.set_label_coords(-0.025,1)
+plt.show()
+
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(
+    units=4,
+    input_shape = (2,),
+    activation = 'relu'))
+model.add(tf.keras.layers.Dense(
+    units=4,
+    activation = 'relu'))
+model.add(tf.keras.layers.Dense(
+    units=1,
+    activation = 'sigmoid'))
+model.summary()
+
+model.compile (optimizer = tf.keras.optimizers.SGD(),
+               loss = tf.keras.losses.BinaryCrossentropy(),
+               metrics=[tf.keras.metrics.BinaryAccuracy()])
+hist = model.fit(x_train,y_train,
+                 validation_data = (x_valid,y_valid),
+                 epochs=200, batch_size = 2, verbose=0)
+
+history = hist.history
+fig = plt.figure(figsize=(16,4))
+ax = fig.add_subplot(1,3,1)
+plt.plot(history['loss'],lw=4)
+plt.plot(history['val_loss'],lw=4)
+plt.legend(['Train loss','Validation loss'],fontsize=15)
+ax.set_xlabel('Epochs',size=15)
+
+ax=fig.add_subplot(1,3,2)
+plt.plot(history['binary_accuracy'],lw=4)
+plt.plot(history['val_binary_accuracy'],lw=4)
+plt.legend(['Train acc.','Validation Acc.'],fontsize=15)
+ax.set_xlabel('Epochs',size=15)
+
+ax=fig.add_subplot(1,3,3)
+plot_decision_regions(X=x_valid,y=y_valid.astype(np.integer),
+                      clf=model)
+ax.set_xlabel(r'$x_1$',size=15)
+ax.xaxis.set_label_coords(1,-0.025)
+ax.set_ylabel(r'$x_2$',size=15)
+ax.yaxis.set_label_coords(-0.025,1)
+plt.show()
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(
+    units=4,
+    input_shape = (2,),
+    activation = 'relu'))
+model.add(tf.keras.layers.Dense(
+    units=4,
+    activation = 'relu'))
+model.add(tf.keras.layers.Dense(
+    units=4,
+    activation = 'relu'))
+model.add(tf.keras.layers.Dense(
+    units=1,
+    activation = 'sigmoid'))
+model.summary()
+
+model.compile (optimizer = tf.keras.optimizers.SGD(),
+               loss = tf.keras.losses.BinaryCrossentropy(),
+               metrics=[tf.keras.metrics.BinaryAccuracy()])
+hist = model.fit(x_train,y_train,
+                 validation_data = (x_valid,y_valid),
+                 epochs=200, batch_size = 2, verbose=0)
+
+history = hist.history
+fig = plt.figure(figsize=(16,4))
+ax = fig.add_subplot(1,3,1)
+plt.plot(history['loss'],lw=4)
+plt.plot(history['val_loss'],lw=4)
+plt.legend(['Train loss','Validation loss'],fontsize=15)
+ax.set_xlabel('Epochs',size=15)
+
+ax=fig.add_subplot(1,3,2)
+plt.plot(history['binary_accuracy'],lw=4)
+plt.plot(history['val_binary_accuracy'],lw=4)
+plt.legend(['Train acc.','Validation Acc.'],fontsize=15)
+ax.set_xlabel('Epochs',size=15)
+
+ax=fig.add_subplot(1,3,3)
+plot_decision_regions(X=x_valid,y=y_valid.astype(np.integer),
+                      clf=model)
+ax.set_xlabel(r'$x_1$',size=15)
+ax.xaxis.set_label_coords(1,-0.025)
+ax.set_ylabel(r'$x_2$',size=15)
+ax.yaxis.set_label_coords(-0.025,1)
+plt.show()
