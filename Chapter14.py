819b681e89acf74d6b62cb1afc192571bb46f476
Sima <sima.soltani@queensu.ca>
2020-04-21 15:21:06 -0400

    Page 509 Chapter14

--- Chapter14.py
+++ Chapter14.py
@@ -614,3 +614,129 @@ ax.set_ylabel(r'$x_2$',size=15)
 ax.yaxis.set_label_coords(-0.025,1)
 plt.show()
 
+# TensorFlow Estimators
+
+import pandas as pd
+dataset_path = tf.keras.utils.get_file("auto-mpg.data",
+                                       ("http://archive.ics.uci.edu/ml/machine-learning"
+                                        "-databases/auto-mpg/auto-mpg.data"))
+column_names =[
+    'MPG', 'Cylinders', 'Displacement',
+    'Horsepower', 'Weight', 'Acceleration',
+    'ModelYear','Origin']
+
+df=pd.read_csv(dataset_path,names=column_names,
+               na_values='?', comment='\t',
+               sep=' ',skipinitialspace=True)
+
+## drop the NA rows
+df=df.dropna()
+df = df.reset_index(drop=True)
+
+#train/test split:
+import sklearn 
+import sklearn.model_selection
+
+df_train,df_test=sklearn.model_selection.train_test_split(df,train_size =0.8)
+train_stats=df_train.describe().transpose()
+
+numeric_column_names = [
+    'Cylinders', 'Displacement',
+    'Horsepower','Weight',
+    'Acceleration']
+
+df_train_norm,df_test_norm=df_train.copy(),df_test.copy()
+
+for col_name in numeric_column_names:
+    mean = train_stats.loc[col_name,'mean']
+    std = train_stats.loc[col_name,'std']
+    df_train_norm.loc[:,col_name]=(
+        df_train_norm.loc[:,col_name]-mean)/std
+    df_test_norm.loc[:,col_name]=(
+        df_test_norm.loc[:,col_name]-mean)/std
+df_train_norm.tail()
+
+numeric_features=[]
+for col_name in numeric_column_names:
+    numeric_features.append(
+        tf.feature_column.numeric_column(key=col_name))
+
+feature_year = tf.feature_column.numeric_column(key='ModelYear')
+bucketized_features=[]
+bucketized_features.append(tf.feature_column.bucketized_column(
+    source_column=feature_year,
+    boundaries=[73,76,79]))
+
+feature_origin = tf.feature_column.categorical_column_with_vocabulary_list(
+    key='Origin',
+    vocabulary_list=[1,2,3])
+
+categorical_indicator_features=[]
+categorical_indicator_features.append(
+    tf.feature_column.indicator_column(feature_origin))
+
+##### Machine Learning with pre-made Estimators
+#Step1 : Define an input function for data loading
+def train_input_fn(df_train,batch_size=8):
+    df= df_train.copy()
+    train_x,train_y = df,df.pop('MPG')
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (dict(train_x),train_y))
+    #shuffle, repeat, and batch the examples
+    return dataset.shuffle(1000).repeat().batch(batch_size)
+
+ #load a batch to see how does it look like
+ds = train_input_fn(df_train_norm)
+batch = next(iter(ds))
+print('Keys:', batch[0].keys())
+
+print('Batch Model Years:', batch[0]['ModelYear'])   
+
+## define an input function for the test dataset that will be used for evaluation after model training
+def eval_input_fn(df_test,batch_size=8):
+    df = df_test.copy()
+    test_x,test_y=df,df.pop('MPG')
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (dict(test_x),test_y))
+    return dataset.batch(batch_size)
+
+#Step2 : Convert the dataset into feature columns
+all_feature_columns =(
+    numeric_features+
+    bucketized_features+
+    categorical_indicator_features)
+
+#Step3: Instantiate an Estimator 
+regressor = tf.estimator.DNNRegressor(feature_columns=all_feature_columns,
+                                      hidden_units=[32,10],
+                                      model_dir='models/autompg-dnnregressor/')
+
+#Step4: Use the Estimator methods train(), evaluate(), and predict()
+
+EPOCHS = 1000
+BATCH_SIZE = 8
+total_steps  = EPOCHS * int(np.ceil(len(df_train)/BATCH_SIZE))
+print('Training Steps:', total_steps)
+
+regressor.train(input_fn=lambda:train_input_fn(
+    df_train_norm,batch_size=BATCH_SIZE),
+    steps=total_steps)
+
+# reload the last checkpoint
+reloaded_regressor = tf.estimator.DNNRegressor(hidden_units=[32,10], 
+                                             feature_columns=all_feature_columns,
+                                             warm_start_from ='models/autompg-dnnregressor/')
+
+# Evaluate the predictive performance of the trained model
+eval_results = reloaded_regressor.evaluate(
+    input_fn=lambda:eval_input_fn(df_test_norm,batch_size=8))
+
+print('Average-Loss {:.4f}'.format(
+    eval_results['average_loss']))
+
+#predict the target valueson new points
+pred_res = regressor.predict(
+    input_fn=lambda:eval_input_fn(df_test_norm, batch_size=8))
+
+print(next(iter(pred_res)))
+
